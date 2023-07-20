59d122ecf9740734f69604cfbccae548c68b5c1a
Dhara-Ghediya <131964916+Dhara-Ghediya@users.noreply.github.com>
2023-07-20 14:36:05 +0530

    Added register, login, logout and profile modules in student_app server side.

--- IELTS_eLearning_serevr/IELTS_eLearning_serevr/settings.py
+++ IELTS_eLearning_serevr/IELTS_eLearning_serevr/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
     'student_app',
     'teacher_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
